714298e4dd893d335f503fe53cf094f59fab6dd1
theNerd247 <harvey.coder247@gmail.com>
2013-10-19 23:14:35 -0400

    added cmdbar * cleaned up some funcs * stoptui now frees memory * keyboard control moved to command bar

--- plugin/tui/src/tui.c
+++ plugin/tui/src/tui.c
@@ -32,10 +32,16 @@
 #include "tui.h"
 #include "dbg.h"
 
+//custom keymaps
+#define ESC_KEY 27
+
 /* menus for the toolbar */
 #define NMENUS 6
 MENU* menus[NMENUS];
 const char* menu_titles[NMENUS] = {"Math","Functions","Tables","Graphs","Settings","Plugin"}; //titles of the table menu
+
+/* command bar */
+WINDOW* cmdbar; 
 
 //helper function for __init_winstructs
 /* initializes all the menus at the top of the screen */
@@ -56,12 +62,13 @@ MENU* __init_menu(size_t ysize, size_t xsize, size_t ypos, size_t xpos, const ch
 	//enable the keypad for the windows
 	keypad(container, TRUE);
 
+	//we want controll over the cursor
+	leaveok(container, FALSE);
+
 	//draw the border and title
-	mvwhline(container,1,0,ACS_HLINE,xsize);
 	mvwprintw(container,0,(xsize/2)-strlen(title)/2,title);
 	wnoutrefresh(container);
 
-		
 	//set the windows and subwindows
 	check_expr(set_menu_win(menu, container), E_OK, "Failed to set window for\
 			menu: %s",title); 
@@ -94,20 +101,44 @@ void __free_menu(MENU* menu)
 		free_menu(menu);
 }
 
+//helper function for __init_winstructs
+/* initializes cmdbar */
+WINDOW* __init_cmdbar()
+{
+	//init window
+	WINDOW* cmdbr;
+	check(cmdbr = newwin(3,COLS,LINES-3,0),"Failed to create command bar");
+
+	//draw the border
+	mvwhline(cmdbr,0,0,ACS_HLINE,COLS);
+	mvwhline(cmdbr,2,0,ACS_HLINE,COLS);
+
+	//we want controll over the cursor
+	leaveok(cmdbr, FALSE);
+
+	//apply updates and return	
+	wnoutrefresh(cmdbr);
+	return cmdbr;
+
+	error:
+		return NULL;
+}
+
 //helper function for starttui
 /* initializes all window structures for screen display*/
 int __init_winstructs()
 {
 	int error_code = 0;
+
 	//initialize menus 
 	size_t i;
 	for (i = 0; i < NMENUS; i++)
-	{
 		error_run(menus[i] = __init_menu(7,COLS/6,0,i*(COLS/6),menu_titles[i]), error_code =1);
-	}
+
 	//initialize displays
 
 	//initialize command bar
+	error_run(cmdbar = __init_cmdbar(), error_code = 2);
 	
 	return 0;
 
@@ -115,6 +146,7 @@ int __init_winstructs()
 		return error_code;
 }
 
+/* sets up and starts running the tui */
 void* starttui(void* null)
 {
 	//vars
@@ -123,19 +155,21 @@ void* starttui(void* null)
 	initscr();
 
 	//keyboard config
-	cbreak();
-	noecho();
-	refresh();
+	cbreak(); //don't wait for a new line character
+	echo(); //print what we type
 	keypad(stdscr, TRUE); //we want to enable F1-F12, etc...
 
 	//create window structs
-	error_code = __init_winstructs();
+	check_expr(__init_winstructs(),0,"Failed to create windows. aborting");
+
+	//move the cursor to the cmdbar
+	wmove(cmdbar,1,0);
 
 	//display the windows
 	doupdate();
 
 	//process keyboard events
-	while(getch() != 'q')
+	while(wgetch(cmdbar) != ESC_KEY)
 		;
 
 	error: 
@@ -143,7 +177,20 @@ void* starttui(void* null)
 		return NULL;
 }
 
+//helper function for stoptui
+/* frees the window structures */
+void __free_winstructs()
+{
+
+}
+
+/* stops running the tui interface */
 void stoptui()
 {
+	//free menus
+	size_t i;
+	for (i = 0; i < NMENUS; i++)
+		__free_menu(menus[i]);
+
 	endwin();
 }
